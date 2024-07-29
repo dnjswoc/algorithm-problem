81eb69fbd455a4aa27c04843117b59adb5f9aed5
dnjswoc <dnjswoc@gmail.com>
2024-07-29 23:53:06 +0900

    16504_update

--- list1/16504_Gravity/16504_Gravity.py
+++ list1/16504_Gravity/16504_Gravity.py
@@ -3,12 +3,10 @@ T = int(input())
 for i in range(T):
     N = int(input())
     num_list = list(map(int, input().split()))
-    count=0
     num_count=0
-    for num in num_list:
-        count+=1
-    for j in range(count):
+    for j in range(N):
         if num_list[j] > 0:
             num_count+=1
+        print(num_count)
     print(num_list)
     # print(f'#{i+1} {answer}')
