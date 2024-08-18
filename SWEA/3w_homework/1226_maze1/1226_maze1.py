48d12214a1f29216cd9e137bc5d25eace4c00290
dnjswoc <dnjswoc@gmail.com>
2024-08-18 21:50:23 +0900

    4408_comeback_room

--- SWEA/3w_homework/1226_maze1/1226_maze1.py
+++ SWEA/3w_homework/1226_maze1/1226_maze1.py
@@ -2,34 +2,34 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 
-def start_idx(arr):
+def start_idx(arr):     # 미로의 시작점을 찾는 함수
     for i in range(16):
         for j in range(16):
-            if arr[i][j] == 2:
+            if arr[i][j] == 2:  # 2이면 시작점이 된다.
                 return i, j
 
 
-def check_maze(arr, x, y):
+def check_maze(arr, x, y):      # 미로가 성립하는지 확인하는 함수
     visited = [[0] * 16 for _ in range(16)]
-    queue = [x, y]
-    while queue:
+    queue = [x, y]  # 시작점을 큐에 입력
+    while queue:    # 큐가 공백상태가 될 때까지 반복
         x, y = queue.pop(0), queue.pop(0)
         if not visited[x][y]:
             visited[x][y] = True
             for di, dj in dij:
                 ni = x + di
                 nj = y + dj
-                if arr[ni][nj] == 3:
+                if arr[ni][nj] == 3:    # 탐색 중 3을 찾으면 미로가 성립
                     return 1
-                if arr[ni][nj] == 1:
+                if arr[ni][nj] == 1:    # 1이면 벽이라 갈 수 없음
                     continue
-                if not visited[ni][nj]:
+                if not visited[ni][nj]:  # 1도 3도 아니고, 방문한 적이 없으면 그 방향으로 나아감
                     queue.append(ni)
                     queue.append(nj)
-    return 0
+    return 0    # 큐가 공백상태가 되도록 3을 찾지 못하면 미로가 성립하지 않음
 
 
-dij = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+dij = [[0, 1], [1, 0], [0, -1], [-1, 0]]    # 델타 탐색(우, 하, 좌, 상)
 
 for test_case in range(10):
     T = int(input())
---
+++ SWEA/IM/4408_comeback_room/4408_comeback_room.py
@@ -0,0 +1,30 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+T = int(input())
+
+for test_case in range(T):
+    N = int(input())
+    return_arr = [list(map(int, input().split())) for _ in range(N)]    # 출발하고 돌아가야 할 학생의 방 번호
+    room_arr = [0] * 200    # 빈 복도의 배열
+    return_cnt = 1  # 최소 시간
+
+    for arr in return_arr:  # 출발하는 방 번호를 더 적은 번호로 놓기 위하여 정렬
+        if arr[0] > arr[1]:
+            arr[0], arr[1] = arr[1], arr[0]
+
+    student_arr = [0] * N
+
+    for i in range(N):
+        start = return_arr[i][0]  # 출발하는 방 번호
+        end = return_arr[i][1]    # 도착하는 방 번호
+        student = list(range(start // 2 + start % 2 - 1, end // 2 + end % 2))  # 방 번호가 짝수, 홀수인 경우를 고려하여
+        student_arr[i] = student    # i번째 학생이 거치는 복도의 인덱스를 리스트에 저장
+        for num in student_arr[i]:  # i번째 학생이 거치는 복도의 인덱스를
+            room_arr[num] += 1  # 비어있는 복도의 인덱스에 하나씩 더해준다
+
+    for j in range(199):    # 학생들이 가장 많이 지나친 복도의 횟수를 구하기 위한 반복문
+        if room_arr[j] >= room_arr[j + 1]:
+            room_arr[j], room_arr[j + 1] = room_arr[j + 1], room_arr[j]
+
+    print(f'#{test_case + 1} {room_arr[-1]}')
