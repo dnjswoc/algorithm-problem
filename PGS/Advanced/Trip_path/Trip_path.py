00feaadcb6ce929411737881a5239721027c4895
dnjswoc <dnjswoc@gmail.com>
2024-09-10 17:57:19 +0900

    240910

--- PGS/Advanced/Trip_path/Trip_path.py
+++ PGS/Advanced/Trip_path/Trip_path.py
@@ -1,25 +1,26 @@
-def dfs(start, dictionary, arr, visited):
+def trip(start, dictionary, arr, visited):
+
     for i in range(len(dictionary[start])):
         if visited[start][i]:
             continue
+        if not dictionary[dictionary[start][i]]:
+            continue
         arr.append(dictionary[start][i])
         visited[start][i] = 1
-        dfs(dictionary[start][i], dictionary, arr, visited)
+        trip(dictionary[start][i], dictionary, arr, visited)
+
     return arr
 
 
 def solution(tickets):
     airport_dict = {}
-    visited = {}
+    visited = {}# 경로 중복도 있음
 
     for ticket in tickets:
         for airport in ticket:
             if airport not in airport_dict:
                 airport_dict.setdefault(airport, [])
                 visited.setdefault(airport, [])
-
-    N = len(airport_dict)
-    print(N)
 
     for ticket in tickets:
         airport_dict[ticket[0]].append(ticket[1])
@@ -30,11 +31,18 @@ def solution(tickets):
 
     print(visited)
     print(airport_dict)
-    path = [tickets[0][0]]
-    answer = dfs(tickets[0][0], airport_dict, path, visited)
+    path = ["ICN"]
+    answer = trip("ICN", airport_dict, path, visited)
+
+    for keys in airport_dict.keys():
+        for i in range(len(airport_dict[keys])):
+            if not visited[keys][i]:
+                answer.append(airport_dict[keys][i])
     return answer
 
 
 # print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
-# print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]))
-print(solution([['icn', 'jfk'], ['jfk', 'pus'], ['pus', 'jej'], ['jej', 'icn']]))
+# print(solution([["SFO", "ATL"], ["ICN", "SFO"], ["ICN", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]))
+# print(solution([["ICN", "AAA"], ["DDD", "ICN"], ["ICN", "DDD"]]))
+# print(solution([["ICN", "AAA"], ["AAA", "ICN"], ["ICN", "CCC"], ["CCC", "ICN"], ["ICN", "DDD"], ["DDD", "AAA"]]))
+print(solution([["ICN", "BBB"], ["BBB", "ICN"], ["ICN", "AAA"]]))
---
+++ PGS/Advanced/Word_change/Word_change.py
@@ -0,0 +1,6 @@
+def solution(begin, target, words):
+    answer = 0
+    return answer
+
+
+print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
